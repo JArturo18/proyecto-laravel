982576d2cef2b4368238a436a792a8a27f58f591
JARTUROFP <36276980+JARTUROFP@users.noreply.github.com>
2022-04-28 14:33:28 -0500

    laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
-use App\Http\Controllers\Dashboard\CategoryController;
-use App\Http\Controllers\Dashboard\PostController;
-//use App\Http\Controllers\Dashboard\TestController;
-//use App\Http\Middleware\TestMiddleware;
-use Illuminate\Support\Facades\Route;
 
-//use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes 
+| Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -18,86 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/',[TestController::class, 'index']);
-//Route::get('/',[App\Http\Controllers\TestController::class, 'test']);
-//Route::get('/',[App\Http\Controllers\TestController::class, 'test']);
-
 Route::get('/', function () {
-return view('welcome');
+    return view('welcome');
 });
 
-Route::middleware([App\Http\Middleware\TestMiddleware::class])->group(function (){
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-    Route::get('/test/{id?}/{name?}', function ($id = 10, $name = " Jose Arturo" ){
-
-            echo $id;
-            echo $name;
-
-});
-});
-
-Route::group(['prefix' => 'dashboard'], function(){
-    
- Route::resource('post', PostController::class)->except(['show']);
-// Route::resource('category', CategoryController::class);
-
-
-//   Rouite::resources([
-//  'post' => PostController::class,
-//  'category' => categoryController::class,
-
-]);
-});
-
-
-// Route::get('/test/{id?}/{name?}', function($id = 10,$name=" Jose Arturo" ){
-
-//     echo $id;
-//     echo $name;
-
-// });
-
-// Route::controller(PostController::class)->group(function(){
-// Route::get('post', 'index')->name("post.index");
-// Route::get('post/{post}', 'show')->name("post.show");
-// Route::get('post/create', 'create')->name("post.create");
-// //Route::get('post/create', [PostController::class, 'create'])->name('post.create');
-// //Route::post('post/create', [PostController::class, 'store'])->name('post.store');
-// Route::get('post/{post}/edit', 'edit')->name("post.edit");
-// Route::post('post', 'store')->name("post.store");
-// Route::put('post/{post}', 'update')->name("post.updatee");
-// Route::delete('post/{post}', 'delete')->name("post.destroy");
-// //Route::delete('post/{post}', [PostController::class, 'update']);
-
-
-// });   
-
-
-
-
-//Route::get('post', [PostController::class, 'index']);
-//Route::get('post/{post}', [PostController::class, 'show']);
-
-// Route::get('post/create', [PostController::class, 'create'])->name('post.create');
-// Route::post('post/create', [PostController::class, 'store'])->name('post.store');
-
-//Route::get('post/{post}/edit', [PostController::class, 'edit']);
-
-//Route::post('post', [PostController::class, 'store']);
-//Route::put('post/{post}', [PostController::class, 'update']);
-//Route::delete('post/{post}', [PostController::class, 'update']);
-
-//Route::get('/contacto', function () {
-//return "Contactame";
-//})->name('contacto');
-
-//Route::get('/custom', function () {
-//$msj2 = "Mensaje desde el servidor *_*";
-
-//$data = ['msj' => $msj2, "edad" => 15];
-//return view('custom',$data);
-//});
-
-
-
-
+require __DIR__.'/auth.php';
